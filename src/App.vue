ee7ab7d46223da01b22dfe9b81327bb205366750
Andreas Schujkow <andreas.schujkow@hivemq.com>
2023-11-23 10:23:43 +0100

    feat: add component update example

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
     <p>Hello World</p>
     <Input />
-    <HmqButton text="Anton" type="button" @click="clickHandler" />
+    <HmqButton :text="text" type="button" @click="clickHandler" />
 </template>
 
 <script setup lang="ts">
 import Input from "./vue-components/Input.vue";
 import HmqButton from "./vue-components/Button"
+import {ref} from "vue";
 
 function clickHandler() {
   console.log('button clicked')
 }
+
+const text = ref('test')
+
+
+window.setInterval(() => {
+  text.value = 'text ' + Date.now().toString()
+}, 1000)
 </script>

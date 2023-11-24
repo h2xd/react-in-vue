bb54e8f405a7717c57d6c9db758337bc3a416127
Andreas Schujkow <andreas.schujkow@hivemq.com>
2023-11-24 15:52:30 +0100

    chore: format file

--- src/utils/dryExample.ts
+++ src/utils/dryExample.ts
@@ -1,28 +1,28 @@
-import {onBeforeUnmount, onMounted, ref} from "vue";
-import {createRoot} from "react-dom/client";
-import React from "react";
+import { onBeforeUnmount, onMounted, ref } from 'vue'
+import { createRoot } from 'react-dom/client'
+import React from 'react'
 
 export function createReactWrapper(options: any) {
-    const root = ref<ReturnType<typeof createRoot>>()
+  const root = ref<ReturnType<typeof createRoot>>()
 
-    function updateReactComponent(props: any) {
-        root.value!.render(React.createElement(options.component, props));
-    }
+  function updateReactComponent(props: any) {
+    root.value!.render(React.createElement(options.component, props))
+  }
 
-    function unmountReactComponent() {
-        root.value?.unmount()
-    }
+  function unmountReactComponent() {
+    root.value?.unmount()
+  }
 
-    onMounted(() => {
-        root.value = createRoot(options.rootElement.value!)
-        updateReactComponent(options.props)
-    });
+  onMounted(() => {
+    root.value = createRoot(options.rootElement.value!)
+    updateReactComponent(options.props)
+  })
 
-    onBeforeUnmount(() => unmountReactComponent);
+  onBeforeUnmount(() => unmountReactComponent)
 
-    return {
-        update: (props: any) => {
-            updateReactComponent(props)
-        }
-    }
+  return {
+    update: (props: any) => {
+      updateReactComponent(props)
+    },
+  }
 }

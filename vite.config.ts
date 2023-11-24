aba23c5c10c1e9feb3c2ce60afb989072fca814f
Andreas Schujkow <andreas.schujkow@hivemq.com>
2023-11-24 11:34:26 +0100

    ci: add whole app path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://h2xd.github.io/',
+  base: 'https://h2xd.github.io/react-in-vue/',
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm-bundler.js'

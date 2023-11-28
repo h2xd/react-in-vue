2cefadd8cbf09a97e19d3b4b4ddea3615e0c96f2
Andreas Schujkow <andreas.schujkow@hivemq.com>
2023-11-28 09:25:46 +0100

    docs: make clear what is react

--- src/demos/components/demo.vue
+++ src/demos/components/demo.vue
@@ -21,27 +21,30 @@ defineEmits(['reset'])
     <slot name="description" />
   </p>
 
+  <div class="font-mono flex flex-row items-center text-xs mb-2 mt-4">
+    <img src="../../public/images/react-logo.svg" width="16" height="16" alt="React.js logo" class="inline-block mr-1">Component written in React.js
+  </div>
   <div class="border-2 rounded border-gray-300 mb-2 dark:border-gray-800">
-    <div class="p-4">
+    <div class="p-4 flex flex-col gap-4">
       <slot name="default" />
     </div>
+  </div>
 
-    <div class="text-xs border-t dark:border-gray-800">
-      <div class="border-b border-gray-200 py-2 px-4 flex flex-row items-center justify-between dark:border-gray-800">
-        <span>
-          <Token href="https://vuejs.org/api/reactivity-core.html#ref" text="ref" />
-          value within
-          <a href="https://vuejs.org/" target="_blank" class="hover:underline hover:text-blue-600">
-            <img src="../../public/images/vue-logo.svg" width="16" height="16" alt="Vue.js logo" class="inline-block">
-            Vue.js
-          </a>
-        </span>
+  <div class="text-xs mt-4">
+    <div class="flex flex-row items-center justify-between dark:border-gray-800 mb-2">
+      <span>
+        <Token href="https://vuejs.org/api/reactivity-core.html#ref" text="ref" />
+        value within
+        <a href="https://vuejs.org/" target="_blank" class="hover:underline hover:text-blue-600">
+          <img src="../../public/images/vue-logo.svg" width="16" height="16" alt="Vue.js logo" class="inline-block">
+          Vue.js
+        </a>
+      </span>
 
-        <Button text="reset" variant="secondary" @click="$emit('reset')" />
-      </div>
-
-      <pre class="p-4 bg-gray-50 dark:bg-gray-900"><code><slot name="code" /></code></pre>
+      <Button text="reset" variant="secondary" @click="$emit('reset')" />
     </div>
+
+    <pre class="p-4 border-2 rounded border-gray-300 mb-2 dark:border-gray-800"><code><slot name="code" /></code></pre>
   </div>
   <Link :href="href" text="View implementation of this demo" class-name="text-sm" />
 </template>
